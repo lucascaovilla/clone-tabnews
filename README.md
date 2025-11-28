07d8a4886ef1943cec5a3132fdffcd26fe8e3f2f
Lucas Grisa Caovilla <114552388+lucascaovilla@users.noreply.github.com>
2025-11-28 16:30:54 +0000

    feat: Installs and sets up prettier linter.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do https://tabnews.com.br para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 function Home() {
-    return <main>
-        <h1>Eu te amo Pimentinha!</h1>
-        <button onClick={() => alert("Vamos comer um sushi hoje?")}>
-            Clique para uma surpresa
-        </button>
+  return (
+    <main>
+      <h1>Eu te amo Pimentinha!</h1>
+      <button onClick={() => alert("Vamos comer um sushi hoje?")}>
+        Clique para uma surpresa
+      </button>
     </main>
-};
+  );
+}
 
 export default Home;
